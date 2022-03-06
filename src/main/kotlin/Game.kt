b09140e51265296a7652eb68139903418562f99b
Ayfri <pierre.ayfri@gmail.com>
2022-03-06 15:34:01 +0100

    feat: Add selection of blocks.

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,4 +1,3 @@
-
 import blocks.Block
 import blocks.BlockState
 import kotlinx.browser.document
@@ -14,12 +13,27 @@ import pixi.typings.ticker.UPDATE_PRIORITY
 import pixi.typings.ticker.ticker
 import pixi.typings.utils.EventEmitter
 import pixi.utils.Application
+import pixi.utils.KeyMap
 import pixi.utils.MouseManager
 
 object Game : EventEmitter() {
 	val blockTextures = mutableMapOf<String, Texture<Resource>>()
+	val keyMap = KeyMap(
+		mapOf(
+			"1" to setOf("1", "&"),
+			"2" to setOf("2", "é"),
+			"3" to setOf("3", "\""),
+			"4" to setOf("4", "'"),
+			"5" to setOf("5", "("),
+			"6" to setOf("6", "-"),
+			"7" to setOf("7", "è"),
+			"8" to setOf("8", "_"),
+			"9" to setOf("9", "ç"),
+		)
+	)
 	val mouseManager = MouseManager()
 	val times = mutableListOf<Double>()
+	var selectedBlock = Block.STONE
 	lateinit var level: Level
 	
 	init {
@@ -50,6 +64,10 @@ object Game : EventEmitter() {
 	fun postInit() {
 		document.addEventListener("contextmenu", Event::preventDefault)
 		level = Level()
+		
+		keyMap.onPress("1") { selectedBlock = Block.STONE }
+		keyMap.onPress("2") { selectedBlock = Block.GRASS }
+		keyMap.onPress("3") { selectedBlock = Block.DIRT }
 	}
 	
 	fun update() {
@@ -60,7 +78,7 @@ object Game : EventEmitter() {
 			level.removeBlockState(blockPos)
 		}
 		if (mouseManager.isPressed(2)) {
-			level.setBlockState(blockPos, BlockState(Block.STONE))
+			level.setBlockState(blockPos, BlockState(selectedBlock))
 		}
 	}
 }
